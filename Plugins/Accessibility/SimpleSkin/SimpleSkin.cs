78c0ecd440079e79416287163ac42207a090d15f
Jean-Loup Kahloun <jean-loup.kahloun@invenietis.com>
2012-08-17 13:38:55 +0200

    sendstring is now must exist and run +
    fixing .iss

--- Plugins/Accessibility/SimpleSkin/SimpleSkin.cs
+++ Plugins/Accessibility/SimpleSkin/SimpleSkin.cs
@@ -29,6 +29,9 @@ namespace SimpleSkin
         bool _viewHidden;
 
         [DynamicService( Requires = RunningRequirement.MustExistAndRun )]
+        public IService<ISendKeyCommandHandlerService> SendStringService { get; set; }
+
+        [DynamicService( Requires = RunningRequirement.MustExistAndRun )]
         public IService<IKeyboardContext> KeyboardContext { get; set; }
 
         [RequiredService]

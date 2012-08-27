c09713391eebb4da2a1ff9a0ce31861c3bbad13b
Jean-Loup Kahloun <jean-loup.kahloun@invenietis.com>
2012-08-27 10:59:03 +0200

    Distribution can now be set via the Host's AssemblyInfo.

--- Host/Properties/AssemblyInfo.cs
+++ Host/Properties/AssemblyInfo.cs
@@ -26,6 +26,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
+using Host.Services.Helpers;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -76,3 +77,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion( "1.0.0.0" )]
 [assembly: AssemblyFileVersion( "1.0.0.0" )]
+
+[assembly: Distribution("Std")]

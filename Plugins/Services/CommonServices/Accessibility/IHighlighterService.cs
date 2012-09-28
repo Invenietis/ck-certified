c224b1ed8c08c20de5183d6a1f3ca4876cc0ecca
Antoine Blanchet <antoine.blanchet@invenietis.com>
2012-09-28 17:14:37 +0200

    Highlight service and model

---
+++ Plugins/Services/CommonServices/Accessibility/IHighlighterService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CK.Plugin;
+using HighlightModel;
+
+namespace CommonServices.Accessibility
+{
+    /// <summary>
+    /// Service that allow other plugins to register themselves in order to be highlighted.
+    /// </summary>
+    public interface IHighlighterService : IDynamicService
+    {
+        /// <summary>
+        /// Register a highlightable tree in the service in order to be available for the highlighting
+        /// </summary>
+        /// <param name="root"></param>
+        void RegisterTree( IHighlightableElement root );
+
+        /// <summary>
+        /// Remove an tree that have been registered before. It can be a subtree of any highlightable element.
+        /// </summary>
+        /// <param name="element"></param>
+        void UnregisterTree( IHighlightableElement element );
+
+        /// <summary>
+        /// Event fired to trigger the highlightment of a particular element (or tree).
+        /// </summary>
+        event EventHandler<HighlightEventArgs> BeginHighlight;
+
+        /// <summary>
+        /// Event fired to end the highlightment of a particular element (or tree).
+        /// </summary>
+        event EventHandler<HighlightEventArgs> EndHighlight;
+    }
+
+    public class HighlightEventArgs : EventArgs
+    {
+        public HighlightEventArgs( IHighlightableElement element )
+        {
+            Element = element;
+        }
+
+        public IHighlightableElement Element { get; private set; }
+    }
+}

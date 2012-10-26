de9039f2f5403d9d6aefcefc617c4046fbea2b2e
Jean-Loup Kahloun <jean-loup.kahloun@invenietis.com>
2012-10-26 12:02:40 +0200

    moving files in the keyboardEditor plugin

---
+++ Plugins/Advanced/ContextEditor/Wizard/IKeyboardEditorRoot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CK.Context;
+using CK.Keyboard.Model;
+using CK.Plugin;
+
+namespace ContextEditor
+{
+    public interface IKeyboardEditorRoot : IKeyboardBackupManager
+    {
+        /// <summary>
+        /// Gets the Keyboardcontext service implementation
+        /// Gives access to all the keyboards and their layouts, zones, keys etc..
+        /// </summary>
+        IService<IKeyboardContext> KeyboardContext { get; }
+
+        /// <summary>
+        /// Gets the Context.
+        /// Gives access to the application's inner management
+        /// </summary>
+        IContext Context { get; }
+    }
+}

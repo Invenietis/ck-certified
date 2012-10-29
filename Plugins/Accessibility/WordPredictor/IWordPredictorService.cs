44ca54bbe9334ab41dea151e3fd2e00934bd4da6
clegendre <cedric.legendre@invenietis.com>
2012-10-29 14:27:00 +0100

    Add WordPredictor plugin and test

---
+++ Plugins/Accessibility/WordPredictor/IWordPredictorService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WordPredictor
+{
+    public interface IWordPredictorService
+    {
+        event EventHandler<WordPredictedEventArgs> WordPredicted;
+
+        void Predict( string word );
+    }
+}
---
+++ Tests/WordPredictorTest/WordPredictorServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+using WordPredictor;
+
+namespace WordPredictorTest
+{
+    public static class TestHelper
+    {
+        //void AttachEvent<TEventArgs>( Func<EventHandler<TEventArgs>> eventArgs )
+        //{
+        //    eventArgs() += ( sender, e ) => { };
+        //}
+    }
+
+    [TestFixture]
+    public class WordPredictorServiceTest
+    {
+
+        [Test]
+        public void When_I_Predict_A_Word_I_Should_Receive_Event()
+        {
+            string letters = "Bon";
+
+            var w = new Mock<IWordPredictorService>();
+
+            w.Setup( e => e.Predict( It.IsAny<string>() ) )
+                .Raises(
+                    s => s.WordPredicted += ( sender, e ) => { },
+                    () => new WordPredictedEventArgs( letters, letters ) )
+                .Verifiable();
+
+            w.Object.WordPredicted += ( sender, e ) =>
+            {
+                Assert.That( e, Is.Not.Null );
+                Assert.That( e.Letters, Is.EqualTo( letters ) );
+                Assert.That( e.WordPredicted, Is.EqualTo( letters ) );
+            };
+
+            w.Object.Predict( letters );
+
+            w.VerifyAll();
+        }
+
+    }
+}

61389f52ecef3a0fbedba663a77548b9dae46f67
clegendre <cedric.legendre@invenietis.com>
2012-11-02 16:44:49 +0100

    Updates existing panel to make them uses the ConfigBase class

--- Host/ViewModels/AutoClickViewModel.cs
+++ Host/ViewModels/AutoClickViewModel.cs
@@ -36,110 +36,60 @@ using CK.Windows.Config;
 
 namespace Host.VM
 {
-    public class AutoClickViewModel : ConfigPage
+    public class AutoClickViewModel : ConfigBase
     {
-        AppViewModel _app;
-        IPluginProxy _acPlugin;
-        Guid _acId;
-        IObjectPluginConfig _config;
-
         public AutoClickViewModel( AppViewModel app )
-            : base( app.ConfigManager )
+            : base( "{989BE0E6-D710-489e-918F-FBB8700E2BB2}", R.AutoClickConfig, app )
         {
-            DisplayName = R.AutoClickConfig;
-            _app = app;
-            _app.ConfigContainer.Changed += new EventHandler<ConfigChangedEventArgs>( OnConfigChanged );
-            _app.PluginRunner.PluginHost.StatusChanged += ( o, e ) => 
-            {
-                if( e.PluginProxy.PluginKey.PluginId == _acId && _acPlugin == null )
-                {
-                    InitializePlugin();
-                }
-
-                NotifyOfPropertyChange( () => ActivateAutoClic );
-                NotifyOfPropertyChange( () => CountDownDuration );
-                NotifyOfPropertyChange( () => TimeBeforeCountDownStarts );
-                NotifyOfPropertyChange( () => ShowMousePanelOption );
-            };
         }
 
-        void InitializePlugin()
+        protected override void NotifyOfPropertiesChange()
         {
-            _acPlugin = _app.PluginRunner.PluginHost.FindLoadedPlugin( _acId, true );
-            if( _acPlugin != null ) _config = _app.ConfigContainer.GetObjectPluginConfig( _app.CivikeyHost.Context.ConfigManager.UserConfiguration, _acPlugin );
+            NotifyOfPropertyChange( () => ActivatePlugin );
+            NotifyOfPropertyChange( () => CountDownDuration );
+            NotifyOfPropertyChange( () => TimeBeforeCountDownStarts );
+            NotifyOfPropertyChange( () => ShowMousePanelOption );
         }
 
-        void OnConfigChanged( object sender, ConfigChangedEventArgs e )
+        protected override void OnConfigChanged( object sender, ConfigChangedEventArgs e )
         {
-            if( e.MultiPluginId.Contains( _acPlugin ) )
-            {
-                NotifyOfPropertyChange( () => CountDownDuration );
-                NotifyOfPropertyChange( () => TimeBeforeCountDownStarts );
-                NotifyOfPropertyChange( () => ShowMousePanelOption );
-            }
-        }
-
-        public bool ActivateAutoClic
-        {
-            get { return _acPlugin != null ? _app.PluginRunner.PluginHost.IsPluginRunning( _acPlugin.PluginKey ) : false; }
-            set
-            {
-                using( var wait = _app.ShowBusyIndicator() )
-                {
-                    if( value )
-                    {
-                        _app.StartPlugin( _acId );
-
-                        if( _acPlugin == null ) _acPlugin = _app.PluginRunner.PluginHost.FindLoadedPlugin( _acId, true );
-                        _config = _acPlugin != null ? _app.ConfigContainer.GetObjectPluginConfig( _app.CivikeyHost.Context.ConfigManager.UserConfiguration, _acPlugin ) : null;
-                    }
-                    else
-                    {
-                        _app.StopPlugin( _acId );
-                    }
-                }
-
-                NotifyOfPropertyChange( () => ActivateAutoClic );
-                NotifyOfPropertyChange( () => CountDownDuration );
-                NotifyOfPropertyChange( () => TimeBeforeCountDownStarts );
-                NotifyOfPropertyChange( () => ShowMousePanelOption );
-            }
+            NotifyOfPropertyChange( () => CountDownDuration );
+            NotifyOfPropertyChange( () => TimeBeforeCountDownStarts );
+            NotifyOfPropertyChange( () => ShowMousePanelOption );
         }
 
         public int CountDownDuration
         {
-            get { return _config != null ? _config.GetOrSet( "CountDownDuration", 2000 ) : 0; }
+            get { return Config != null ? Config.GetOrSet( "CountDownDuration", 2000 ) : 0; }
             set
             {
-                if( _config != null ) _config.Set( "CountDownDuration", value );
+                if( Config != null ) Config.Set( "CountDownDuration", value );
             }
         }
 
         public int TimeBeforeCountDownStarts
         {
-            get { return _config != null ? _config.GetOrSet( "TimeBeforeCountDownStarts", 1500 ) : 0; }
+            get { return Config != null ? Config.GetOrSet( "TimeBeforeCountDownStarts", 1500 ) : 0; }
             set
             {
-                if( _config != null ) _config.Set( "TimeBeforeCountDownStarts", value );
+                if( Config != null ) Config.Set( "TimeBeforeCountDownStarts", value );
             }
         }
 
         public bool ShowMousePanelOption
         {
-            get { return _config != null ? _config.GetOrSet( "ShowMousePanelOption", false ) : false; }
+            get { return Config != null ? Config.GetOrSet( "ShowMousePanelOption", false ) : false; }
             set
             {
-                if( _config != null ) _config.Set( "ShowMousePanelOption", value );
+                if( Config != null ) Config.Set( "ShowMousePanelOption", value );
             }
         }
 
         protected override void OnInitialize()
         {
-            _acId = new Guid( "{989BE0E6-D710-489e-918F-FBB8700E2BB2}" );
+            base.OnInitialize();
 
-            InitializePlugin();
-
-            var g = this.AddActivableSection( R.AutoClickSectionName.ToLower(), R.AutoClickConfig, this, h => h.ActivateAutoClic, this );
+            var g = this.AddActivableSection( R.AutoClickSectionName.ToLower(), R.AutoClickConfig );
 
             ConfigItemMillisecondProperty p2 = new ConfigItemMillisecondProperty( ConfigManager, this, CK.Reflection.ReflectionHelper.GetPropertyInfo( this, h => h.TimeBeforeCountDownStarts ) );
             p2.DisplayName = R.AutoClickTimeBeforeCountDownStarts;
@@ -150,8 +100,6 @@ namespace Host.VM
             g.Items.Add( p );
 
             g.AddProperty( R.AutoClickShowMousePanelOption, this, h => ShowMousePanelOption );
-
-            base.OnInitialize();
         }
     }
 }

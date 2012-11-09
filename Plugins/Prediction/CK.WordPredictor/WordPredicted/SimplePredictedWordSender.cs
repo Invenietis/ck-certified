83ca68773a1a00f657101cd451b7dacd5d8ee4e3
clegendre <cedric.legendre@invenietis.com>
2012-11-09 14:15:23 +0100

    Sends the word only if it is greater than the offset

--- Plugins/Prediction/CK.WordPredictor/WordPredicted/SimplePredictedWordSender.cs
+++ Plugins/Prediction/CK.WordPredictor/WordPredicted/SimplePredictedWordSender.cs
@@ -31,11 +31,14 @@ namespace CK.WordPredictor
         {
             if( TextualContextService.Service != null && SendStringService.Service != null )
             {
-                string wordToSend = e.Word.Substring( TextualContextService.Service.CaretOffset, e.Word.Length - TextualContextService.Service.CaretOffset );
+                if( e.Word.Length > 0 && e.Word.Length > TextualContextService.Service.CaretOffset )
+                {
+                    string wordToSend = e.Word.Substring( TextualContextService.Service.CaretOffset, e.Word.Length - TextualContextService.Service.CaretOffset );
 
-                if( Feature.InsertSpaceAfterPredictedWord ) wordToSend += " ";
+                    if( Feature.InsertSpaceAfterPredictedWord ) wordToSend += " ";
 
-                SendStringService.Service.SendString( wordToSend );
+                    SendStringService.Service.SendString( wordToSend );
+                }
             }
         }
 

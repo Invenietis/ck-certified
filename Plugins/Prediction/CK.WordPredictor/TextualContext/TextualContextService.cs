81b5a28c51e5a44911f4b5a8122370f3491e9a52
clegendre <cedric.legendre@invenietis.com>
2012-11-13 13:55:25 +0100

    Fix multiple notifying during the same operation.

--- Plugins/Prediction/CK.WordPredictor/TextualContext/TextualContextService.cs
+++ Plugins/Prediction/CK.WordPredictor/TextualContext/TextualContextService.cs
@@ -20,7 +20,7 @@ namespace CK.WordPredictor
 
         [DynamicService( Requires = RunningRequirement.MustExistAndRun )]
         public IService<ISendStringService> SendStringService { get; set; }
-        
+
         [DynamicService( Requires = RunningRequirement.OptionalTryStart )]
         public IService<ISendKeyCommandHandlerService> SendKeyService { get; set; }
 
@@ -50,7 +50,7 @@ namespace CK.WordPredictor
                 CommandTextualContextService.Service.TextualContextClear += OnTextualContextClear;
             }
             CommandTextualContextService.ServiceStatusChanged += OnCommandTextualContextServiceServiceStatusChanged;
-            
+
             if( SendKeyService != null && SendKeyService.Service != null )
             {
                 SendKeyService.Service.KeySent += OnKeySent;
@@ -72,7 +72,7 @@ namespace CK.WordPredictor
                 CommandTextualContextService.Service.TextualContextClear -= OnTextualContextClear;
             }
             CommandTextualContextService.ServiceStatusChanged -= OnCommandTextualContextServiceServiceStatusChanged;
-            
+
             if( SendKeyService != null && SendKeyService.Service != null )
             {
                 SendKeyService.Service.KeySent -= OnKeySent;
@@ -99,12 +99,15 @@ namespace CK.WordPredictor
         {
             if( e.StringVal != null ) SetToken( e.StringVal );
         }
-        
+
         void SetToken( string token )
         {
             string newRawContext = String.Concat( _rawContext, token );
-            SetRawText( newRawContext );
-            SetCaretIndex( newRawContext.Length );
+
+            _caretIndex = newRawContext.Length;
+            InternalSetRawText( newRawContext );
+
+            NotifyPropertiesChanged( "CurrentToken", "Tokens", "CurrentTokenIndex", "CaretOffset", "CurrentPosition" );
         }
 
         protected virtual void OnTextualContextClear( object sender, EventArgs e )
@@ -114,12 +117,7 @@ namespace CK.WordPredictor
             _caretIndex = 0;
             _tokenSeparatorIndexes = new int[0];
 
-            OnPropertyChanged( "Tokens" );
-            OnPropertyChanged( "RawContext" );
-            OnPropertyChanged( "CurrentToken" );
-            OnPropertyChanged( "CurrentTokenIndex" );
-            OnPropertyChanged( "CurrentPosition" );
-            OnPropertyChanged( "CaretOffset" );
+            NotifyPropertiesChanged( "CurrentToken", "Tokens", "CurrentTokenIndex", "CaretOffset", "CurrentPosition" );
         }
 
         private void OnPredictionAreaContentSent( object sender, PredictionAreaContentEventArgs e )
@@ -218,15 +216,11 @@ namespace CK.WordPredictor
             }
         }
 
-
         public void SetCaretIndex( int caretGlobalIndex )
         {
             _caretIndex = caretGlobalIndex;
 
-            OnPropertyChanged( "CurrentToken" );
-            OnPropertyChanged( "CurrentTokenIndex" );
-            OnPropertyChanged( "CurrentPosition" );
-            OnPropertyChanged( "CaretOffset" );
+            NotifyPropertiesChanged( "CurrentToken", "CurrentTokenIndex", "CaretOffset", "CurrentPosition" );
         }
 
         /// <summary>
@@ -240,33 +234,43 @@ namespace CK.WordPredictor
         // WORD1  WORD2 WORD3
         public void SetRawText( string value )
         {
+            if( value == _rawContext ) return;
+
+            InternalSetRawText( value );
+
+            NotifyPropertiesChanged( "CurrentToken", "Tokens" );
+        }
+
+        private void InternalSetRawText( string value )
+        {
+
             _rawContext = value;
 
             if( String.IsNullOrWhiteSpace( value ) )
             {
                 _tokenSeparatorIndexes = new int[0];
-                return;
+                _tokenCollection.Clear( false );
             }
-
-            string[] tokens = Normalization( value ); ;
-            if( tokens.Length > 1 )
+            else
             {
-                _tokenSeparatorIndexes = new int[tokens.Length - 1];
-                _tokenSeparatorIndexes[0] = tokens[0].Length + 1;
 
-                for( int i = 1; i < _tokenSeparatorIndexes.Length; i++ )
+                string[] tokens = Normalization( value ); ;
+                if( tokens.Length > 1 )
                 {
-                    // + 1 for whitespace
-                    _tokenSeparatorIndexes[i] = _tokenSeparatorIndexes[i - 1] + 1 + tokens[i].Length; // The index of the whitespace
+                    _tokenSeparatorIndexes = new int[tokens.Length - 1];
+                    _tokenSeparatorIndexes[0] = tokens[0].Length + 1;
+
+                    for( int i = 1; i < _tokenSeparatorIndexes.Length; i++ )
+                    {
+                        // + 1 for whitespace
+                        _tokenSeparatorIndexes[i] = _tokenSeparatorIndexes[i - 1] + 1 + tokens[i].Length; // The index of the whitespace
+                    }
                 }
+
+                _tokenCollection.Clear( false );
+                _tokenCollection.AddRange( tokens, false );
+
             }
-
-            _tokenCollection.Clear( false );
-            _tokenCollection.AddRange( tokens, false );
-
-            OnPropertyChanged( "RawContext" );
-            OnPropertyChanged( "Tokens" );
-            OnPropertyChanged( "CurrentToken" );
         }
 
         #endregion
@@ -334,6 +338,14 @@ namespace CK.WordPredictor
             if( e.Current == RunningStatus.Starting )
             {
                 SendKeyService.Service.KeySent += OnKeySent;
+            }
+        }
+
+        private void NotifyPropertiesChanged( params string[] properties )
+        {
+            foreach( string p in properties )
+            {
+                OnPropertyChanged( p );
             }
         }
 

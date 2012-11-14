2b59c50a8a307a661b47d269229ed3153fd107eb
clegendre <cedric.legendre@invenietis.com>
2012-11-14 19:08:59 +0100

    Wiazrd First Commit

---
+++ Plugins/WizardFirstStart/ViewModels/HomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CK.WPF.Wizard;
+
+namespace CK.Plugins.WizardFirstStart.ViewModels
+{
+    class HomeViewModel : WizardPage
+    {
+        private readonly WizardManager _wizardManager;
+
+        public HomeViewModel( WizardManager wizardManager )
+            : base( wizardManager, isLastStep: false )
+        {
+            _wizardManager = wizardManager;
+            
+        }
+    }
+}
---
+++ Plugins/WizardFirstStart/ViewModels/WizardViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+using CK.Plugins.WizardFirstStart.Resources;
+using CK.WPF.Wizard;
+
+namespace CK.Plugins.WizardFirstStart.ViewModels
+{
+    class WizardViewModel : Conductor<IScreen>
+    {
+        public WizardViewModel()
+        {
+            DisplayName = R.WindowTitle;
+
+            WizardManager wizardManager = new WizardManager( this );
+            wizardManager.ActivateItem( new HomeViewModel( wizardManager) );
+            ActivateItem( wizardManager );
+        }
+    }
+}

97e2cf3be8c8bb4f6da5bf5ca243bcd739866c96
Antoine Blanchet <antoine.blanchet@invenietis.com>
2012-12-20 15:58:03 +0100

    Plugins can now get their help content asynchronously or just call "show" to open it in a web browser

--- Plugins/OnlineHelp/OnlineHelp.cs
+++ Plugins/OnlineHelp/OnlineHelp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,19 +21,35 @@ namespace OnlineHelp
         Guid PluginGuid = new Guid( PluginGuidString );
         const string PluginIdVersion = "1.0.0";
         const string PluginPublicName = "OnlineHelp";
-        public readonly INamedVersionedUniqueId PluginId;
+        public readonly INamedVersionedUniqueId PluginId = new SimpleNamedVersionedUniqueId( PluginGuidString, PluginIdVersion, PluginPublicName );
+
+        public string _defaultCulture = "en";
 
         [RequiredService]
         public IHostInformation HostInformations { get; set; }
 
-        private string LocalHelpBaseDirectory { get { return HostInformations.ApplicationDataPath; } }
+        private string LocalHelpBaseDirectory { get { return Path.Combine( HostInformations.ApplicationDataPath, "HelpContents" ); } }
 
-        string GetHelpContentFilePath( INamedVersionedUniqueId pluginName )
+        private string NoContentFilePath { get { return Path.Combine( LocalHelpBaseDirectory, "Default", "nocontent.html" ); } }
+
+        string GetHelpLocalContentFilePath( INamedVersionedUniqueId pluginName, string culture = null )
         {
-            string localhelp = Path.Combine( LocalHelpBaseDirectory, pluginName.UniqueId.ToString( "B" ), pluginName.Version.ToString(), "index.html" );
+            if( culture == null ) culture = CultureInfo.CurrentCulture.TwoLetterISOLanguageName.ToLowerInvariant();
+            
+            // try to load the help of the plugin in the good culture (current or given)
+            string localhelp = Path.Combine( LocalHelpBaseDirectory, pluginName.UniqueId.ToString( "B" ), pluginName.Version.ToString(), culture, "index.html" );
             if( !File.Exists( localhelp ) )
             {
-                localhelp = Path.Combine( LocalHelpBaseDirectory, "Default", "nocontent.html" );
+                // if the help does not exists, and if the given culture is already the default, there is no help content
+                if( culture == _defaultCulture )
+                    localhelp = NoContentFilePath;
+                else
+                {
+                    // if the given culture is still not the default, and a specialized culture, try to load the help for the base culture
+                    if( culture.Contains( '-' ) ) return GetHelpLocalContentFilePath( pluginName, culture.Substring( culture.IndexOf( '-' ) ) );
+                    else return GetHelpLocalContentFilePath( pluginName, _defaultCulture );
+                }
+
             }
 
             return localhelp;
@@ -55,12 +72,12 @@ namespace OnlineHelp
         {
         }
 
-        public CancellationTokenSource GetHelpFor( INamedVersionedUniqueId pluginName, Action<Task<string>> onComplete )
+        public CancellationTokenSource GetHelpContentFor( INamedVersionedUniqueId pluginName, Action<Task<string>> onComplete )
         {
             CancellationTokenSource cancellationToken = new CancellationTokenSource();
             Task<string> getContent = new Task<string>( () =>
             {
-                using( StreamReader rdr = new StreamReader( GetHelpContentFilePath( pluginName ) ) )
+                using( StreamReader rdr = new StreamReader( GetHelpLocalContentFilePath( pluginName ) ) )
                 {
                     return rdr.ReadToEnd();
                 }
@@ -71,6 +88,18 @@ namespace OnlineHelp
             getContent.Start();
 
             return cancellationToken;
+        }
+
+        public bool ShowHelpFor( INamedVersionedUniqueId pluginName, bool force = false )
+        {
+            string url = GetHelpLocalContentFilePath(pluginName);
+            bool found = url != NoContentFilePath;
+            if( found || force )
+            {
+                var startinfo = new System.Diagnostics.ProcessStartInfo( "IExplore.exe", url );
+                System.Diagnostics.Process.Start( startinfo );
+            }
+            return found;
         }
     }
 }
--- Plugins/Services/CommonServices/Accessibility/IHelpService.cs
+++ Plugins/Services/CommonServices/Accessibility/IHelpService.cs
@@ -17,6 +17,14 @@ namespace CommonServices.Accessibility
         /// <param name="pluginName">Plugin name</param>
         /// <param name="onComplete">delegate invoked when the help will be loaded</param>
         /// <returns>A cancellation token used to cancel the loading if needed</returns>
-        CancellationTokenSource GetHelpFor( INamedVersionedUniqueId pluginName, Action<Task<string>> onComplete );
+        CancellationTokenSource GetHelpContentFor( INamedVersionedUniqueId pluginName, Action<Task<string>> onComplete );
+
+        /// <summary>
+        /// Open the default web browser to the plugin's help
+        /// </summary>
+        /// <param name="pluginName">The plugin name in order to find the help</param>
+        /// <param name="force">Set force to true to open the web browser even is the content was not found</param>
+        /// <returns>True if the content was found, false otherwise</returns>
+        bool ShowHelpFor( INamedVersionedUniqueId pluginName, bool force = false );
     }
 }

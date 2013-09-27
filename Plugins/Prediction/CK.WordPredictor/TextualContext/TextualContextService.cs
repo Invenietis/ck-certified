d6e931833fea5113c8836c1d250d11188adf807d
Jean-Loup Kahloun <jean-loup.kahloun@invenietis.com>
2013-09-27 14:18:05 +0200

    the word predictor now reacts to the new sendkey service

--- Plugins/Prediction/CK.WordPredictor/TextualContext/TextualContextService.cs
+++ Plugins/Prediction/CK.WordPredictor/TextualContext/TextualContextService.cs
@@ -65,6 +65,7 @@ namespace CK.WordPredictor
 
             if( SendStringService != null && SendStringService.Service != null )
             {
+                SendStringService.Service.KeySent += OnKeySent;
                 SendStringService.Service.StringSent += OnStringSent;
             }
             SendStringService.ServiceStatusChanged += OnSendStringServiceStatusChanged;
@@ -113,6 +114,23 @@ namespace CK.WordPredictor
                     SetRawText( raw );
                 }
                 else if( e.Key == "{ENTER}" )
+                {
+                    ClearContext();
+                }
+            }
+        }
+
+        protected virtual void OnKeySent( object sender, NativeKeySentEventArgs e )
+        {
+            if( !PredictionTextAreaService.Service.IsDriven )
+            {
+                if( e.Key == NativeMethods.KeyboardKeys.Space && _rawContext != null && _rawContext.Length > 0 )
+                {
+                    var raw = _rawContext.Substring( 0, _rawContext.Length - 1 );
+                    _caretIndex = raw.Length;
+                    SetRawText( raw );
+                }
+                else if( e.Key == NativeMethods.KeyboardKeys.Enter )
                 {
                     ClearContext();
                 }
@@ -379,10 +397,12 @@ namespace CK.WordPredictor
         {
             if( e.Current == InternalRunningStatus.Stopping )
             {
+                SendStringService.Service.KeySent -= OnKeySent;
                 SendStringService.Service.StringSent -= OnStringSent;
             }
             if( e.Current == InternalRunningStatus.Starting )
             {
+                SendStringService.Service.KeySent += OnKeySent;
                 SendStringService.Service.StringSent += OnStringSent;
             }
         }

cbda2fb2697c4d53e2ee9086ac5557ed7e8e09d6
Cédric Legendre <cedric.legendre@invenietis.com>
2013-10-16 17:01:10 +0200

    Autobinding OK

--- Plugins/Accessibility/CK.WindowManager/WindowAutoBinder.cs
+++ Plugins/Accessibility/CK.WindowManager/WindowAutoBinder.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
-using System.Drawing;
 using CK.Plugin;
 using CK.WindowManager.Model;
 using CommonServices;
@@ -11,90 +10,83 @@ using System.Diagnostics;
 
 namespace CK.WindowManager
 {
-    [Plugin("{B63BB144-1C13-4A3B-93BD-AC5233F4F18E}", PublicName = "CK.WindowManager.AutoBinder")]
+    [Plugin( "{B63BB144-1C13-4A3B-93BD-AC5233F4F18E}", PublicName = "CK.WindowManager.AutoBinder" )]
     public class WindowAutoBinder : IPlugin
     {
-        [DynamicService(Requires = RunningRequirement.MustExistTryStart)]
+        [DynamicService( Requires = RunningRequirement.MustExistTryStart )]
         public IService<IWindowManager> WindowManager { get; set; }
 
-        [DynamicService(Requires = RunningRequirement.MustExistTryStart)]
+        [DynamicService( Requires = RunningRequirement.MustExistTryStart )]
         public IService<IWindowBinder> WindowBinder { get; set; }
 
-        [DynamicService(Requires = RunningRequirement.MustExistTryStart)]
+        [DynamicService( Requires = RunningRequirement.MustExistTryStart )]
         public IPointerDeviceDriver PointerDeviceDriver { get; set; }
 
-        IDictionary<IWindowElement, RectangleF> _rect;
+        IDictionary<IWindowElement, Rect> _rect;
         bool _binding = false;
 
         public float AttractionRadius = 50;
 
         bool _intersectionDetected = false;
-        bool _processing;
+        bool _previewingBinding;
+        IBindResult _bindResult;
 
-        void OnWindowMoved(object sender, WindowElementLocationEventArgs e)
+        void OnWindowMoved( object sender, WindowElementLocationEventArgs e )
         {
             // Do not act when a binding is occuring.
-            if (_binding == true) return;
+            if( _binding == true ) return;
 
             _intersectionDetected = false;
 
-            ISpatialBinding binding = WindowBinder.Service.GetBinding(e.Window);
-            if (binding == null) return;
+            ISpatialBinding binding = WindowBinder.Service.GetBinding( e.Window );
+            if( binding == null ) return;
 
             IReadOnlyList<IWindowElement> registeredElements = WindowManager.Service.WindowElements;
             ICollection<IWindowElement> boundWindows = binding.AllDescendants().ToArray();
 
-            RectangleF rect = _rect[e.Window] = new RectangleF((float)e.Window.Left, (float)e.Window.Top, (float)e.Window.Width, (float)e.Window.Height);
-            RectangleF enlargedRectangle = RectangleF.Inflate(rect, AttractionRadius, AttractionRadius);
+            Rect rect = _rect[e.Window] = new Rect( e.Window.Left, e.Window.Top, e.Window.Width, e.Window.Height );
+            Rect enlargedRectangle = Rect.Inflate( rect, AttractionRadius, AttractionRadius );
 
             int i = 0;
             IWindowElement otherWindow = null;
 
-            while (i < registeredElements.Count && _intersectionDetected == false)
+            while( i < registeredElements.Count && _intersectionDetected == false )
             {
                 otherWindow = registeredElements[i];
                 // If in all registered windows a window intersect with the one that moved
-                if (otherWindow != e.Window && !boundWindows.Contains(otherWindow))
+                if( otherWindow != e.Window && !boundWindows.Contains( otherWindow ) )
                 {
                     rect = _rect[otherWindow];
-                    _intersectionDetected = rect.IntersectsWith(enlargedRectangle);
+                    _intersectionDetected = rect.IntersectsWith( enlargedRectangle );
                 }
                 i++;
             }
 
-            if (_intersectionDetected)
+            if( _intersectionDetected )
             {
-                Debug.Assert(otherWindow != null);
+                Debug.Assert( otherWindow != null );
                 // Determines the intersection
-                RectangleF overlapRectangle = RectangleF.Intersect(enlargedRectangle, rect);
+                Rect overlapRectangle = Rect.Intersect( enlargedRectangle, rect );
                 BindingPosition pos = BindingPosition.None;
 
-                if (overlapRectangle.Bottom == enlargedRectangle.Bottom) pos = BindingPosition.Top;
-                else if (overlapRectangle.Top == enlargedRectangle.Top) pos = BindingPosition.Bottom;
-                else if (overlapRectangle.Right == enlargedRectangle.Right) pos = BindingPosition.Left;
-                else if (overlapRectangle.Left == enlargedRectangle.Left) pos = BindingPosition.Right;
+                if( overlapRectangle.Bottom == enlargedRectangle.Bottom ) pos = BindingPosition.Top;
+                else if( overlapRectangle.Top == enlargedRectangle.Top ) pos = BindingPosition.Bottom;
+                else if( overlapRectangle.Right == enlargedRectangle.Right ) pos = BindingPosition.Left;
+                else if( overlapRectangle.Left == enlargedRectangle.Left ) pos = BindingPosition.Right;
 
-                if (_previewingBinding == false) _bindResult = WindowBinder.Service.PreviewBind(otherWindow, e.Window, pos);
-
+                _bindResult = WindowBinder.Service.PreviewBind( otherWindow, e.Window, pos );
             }
-            else _bindResult = WindowBinder.Service.PreviewUnbind(otherWindow, e.Window);
+            else _bindResult = WindowBinder.Service.PreviewUnbind( otherWindow, e.Window );
         }
 
-        bool _previewingBinding;
-        IBindResult _bindResult;
-
-        void WindowBinder_PreviewBinding(object sender, WindowBindedEventArgs e)
+        void WindowBinder_PreviewBinding( object sender, WindowBindedEventArgs e )
         {
-            if (e.BindingType == BindingEventType.Attach)
-            {
-                if (_previewingBinding == false) _previewingBinding = true;
-            }
-            else _previewingBinding = false;
+            _previewingBinding = e.BindingType == BindingEventType.Attach && _previewingBinding == false;
         }
 
-        private void PointerDeviceDriver_PointerButtonUp(object sender, PointerDeviceEventArgs e)
+        private void PointerDeviceDriver_PointerButtonUp( object sender, PointerDeviceEventArgs e )
         {
-            if (_intersectionDetected && _bindResult != null)
+            if( _intersectionDetected && _bindResult != null )
             {
                 _bindResult.Seal();
                 _bindResult = null;
@@ -102,32 +94,35 @@ namespace CK.WindowManager
             }
         }
 
-        void Service_BeforeBinding(object sender, WindowBindingEventArgs e)
+        void Service_BeforeBinding( object sender, WindowBindingEventArgs e )
         {
             _binding = true;
         }
 
-        void Service_AfterBinding(object sender, WindowBindedEventArgs e)
+        void Service_AfterBinding( object sender, WindowBindedEventArgs e )
         {
             _binding = false;
         }
 
-        void Service_Registered(object sender, WindowElementEventArgs e)
+        void Service_Registered( object sender, WindowElementEventArgs e )
         {
-            RegisterWindow(e.Window);
+            RegisterWindow( e.Window );
         }
 
-        private void RegisterWindow(IWindowElement window)
+        private void RegisterWindow( IWindowElement window )
         {
-            var rect = new RectangleF((float)window.Left, (float)window.Top, (float)window.Width, (float)window.Height);
-            _rect.Add(window, rect);
+            if( !_rect.ContainsKey( window ) )
+            {
+                var rect = new Rect( window.Left, window.Top, window.Width, window.Height );
+                _rect.Add( window, rect );
+            }
         }
 
         #region IPlugin Members
 
-        public bool Setup(IPluginSetupInfo info)
+        public bool Setup( IPluginSetupInfo info )
         {
-            _rect = new Dictionary<IWindowElement, RectangleF>();
+            _rect = new Dictionary<IWindowElement, Rect>();
             return true;
         }
 
@@ -142,7 +137,7 @@ namespace CK.WindowManager
 
             PointerDeviceDriver.PointerButtonUp += PointerDeviceDriver_PointerButtonUp;
 
-            foreach (IWindowElement e in WindowManager.Service.WindowElements) RegisterWindow(e);
+            foreach( IWindowElement e in WindowManager.Service.WindowElements ) RegisterWindow( e );
         }
 
         public void Stop()

7fda6904608cf8fe4d5fa6076e0390f9f49be2e1
Cédric Legendre <cedric.legendre@invenietis.com>
2013-10-17 17:00:48 +0200

    PreviewBindingInfo class.

---
+++ Library/CK.WindowManager.Model/BindingExtensions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace CK.WindowManager.Model
+{
+
+    public static class BindingExtensions
+    {
+
+        /// <summary>
+        /// Gets all descendants
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<IWindowElement> AllDescendants( this ISpatialBinding me )
+        {
+            if( me.Top != null ) foreach( var e in me.Top.Descendants( me.Window ) ) yield return e;
+            if( me.Left != null ) foreach( var e in me.Left.Descendants( me.Window ) ) yield return e;
+            if( me.Bottom != null ) foreach( var e in me.Bottom.Descendants( me.Window ) ) yield return e;
+            if( me.Right != null ) foreach( var e in me.Right.Descendants( me.Window ) ) yield return e;
+        }
+
+        public static IEnumerable<IWindowElement> AllDescendants( this ISpatialBinding me, BindingPosition excludes )
+        {
+            List<IWindowElement> toExclude = new List<IWindowElement>();
+
+            if( excludes == BindingPosition.Top )
+                foreach( var e in me.Top.Descendants( me.Window ) ) toExclude.Add( e );
+            if( excludes == BindingPosition.Bottom )
+                foreach( var e in me.Bottom.Descendants( me.Window ) ) toExclude.Add( e );
+            if( excludes == BindingPosition.Left )
+                foreach( var e in me.Left.Descendants( me.Window ) ) toExclude.Add( e );
+            if( excludes == BindingPosition.Right )
+                foreach( var e in me.Right.Descendants( me.Window ) ) toExclude.Add( e );
+
+            return me.AllDescendants().Except( toExclude );
+        }
+
+        private static IEnumerable<IWindowElement> Descendants( this ISpatialBinding me, IWindowElement reference )
+        {
+            if( me.Top != null && me.Top.Window != reference ) foreach( var e in me.Top.Descendants( me.Window ) ) yield return e;
+            if( me.Left != null && me.Left.Window != reference ) foreach( var e in me.Left.Descendants( me.Window ) ) yield return e;
+            if( me.Bottom != null && me.Bottom.Window != reference ) foreach( var e in me.Bottom.Descendants( me.Window ) ) yield return e;
+            if( me.Right != null && me.Right.Window != reference ) foreach( var e in me.Right.Descendants( me.Window ) ) yield return e;
+
+            yield return me.Window;
+        }
+
+
+        public static Rect GetWindowArea( this IBinding binding )
+        {
+            if( binding.Position == BindingPosition.Top )
+            {
+                double top = binding.Master.Top - binding.Slave.Height;
+                double left = binding.Master.Left;
+                double width = binding.Master.Width;
+                double height = binding.Slave.Height;
+                return new Rect( left, top, width, height );
+            }
+            if( binding.Position == BindingPosition.Bottom )
+            {
+                double top = binding.Master.Top + binding.Master.Height;
+                double left = binding.Master.Left;
+                double width = binding.Master.Width;
+                double height = binding.Slave.Height;
+                return new Rect( left, top, width, height );
+            }
+            if( binding.Position == BindingPosition.Left )
+            {
+                double top = binding.Master.Top;
+                double left = binding.Master.Left - binding.Slave.Width;
+                double width = binding.Slave.Width;
+                double height = binding.Master.Height;
+                return new Rect( left, top, width, height );
+            }
+            if( binding.Position == BindingPosition.Right )
+            {
+                double top = binding.Master.Top;
+                double left = binding.Master.Left + binding.Master.Width;
+                double width = binding.Slave.Width;
+                double height = binding.Master.Height;
+                return new Rect( left, top, width, height );
+            }
+            return Rect.Empty;
+        }
+    }
+}
---
+++ Library/CK.WindowManager.Model/PreviewBindingInfo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using CK.Windows;
+
+namespace CK.WindowManager.Model
+{
+    public class PreviewBindingInfo
+    {
+        public IBinding Binding;
+        public CKWindow Window;
+
+        public BindingPosition Position
+        {
+            get { return Binding != null ? Binding.Position : BindingPosition.None; }
+        }
+
+        public bool HasPreview
+        {
+            get { return Position != BindingPosition.None; }
+        }
+
+        public void Shutdown()
+        {
+            if( HasPreview )
+            {
+                Binding = null;
+                Window.Dispatcher.BeginInvoke( new Action( () => Window.Hide() ) );
+            }
+        }
+
+        public void Display( IBinding binding )
+        {
+            if( binding == null ) return;
+
+            if( Window == null )
+            {
+                Window = new CKWindow();
+            }
+            Binding = binding;
+
+            Rect r = Binding.GetWindowArea();
+            if( r != Rect.Empty )
+            {
+                Window.Dispatcher.BeginInvoke( new Action( () =>
+                {
+
+                    Window.Opacity = .8;
+                    Window.Background = new System.Windows.Media.SolidColorBrush( System.Windows.Media.Color.FromRgb( 152, 120, 152 ) );
+                    Window.ResizeMode = ResizeMode.NoResize;
+                    Window.WindowStyle = WindowStyle.None;
+                    Window.Show();
+                    Window.Left = r.Left;
+                    Window.Top = r.Top;
+                    Window.Width = r.Width;
+                    Window.Height = r.Height;
+                } ) );
+            }
+        }
+
+        public bool IsPreviewOf( IBinding newBinding )
+        {
+            if( newBinding == null ) throw new ArgumentNullException( "binding" );
+
+            if( Binding == null ) return false;
+            return newBinding.Position == Binding.Position && Binding.Master == newBinding.Master;
+        }
+    }
+
+}

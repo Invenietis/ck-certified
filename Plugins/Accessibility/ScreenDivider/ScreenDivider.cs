882250727f7ecd44d6d7a40ecfd8ad403b3024ae
Julien Mathon <julien.mathon@invenietis.com>
2013-10-24 11:04:39 +0200

    Prepare the new project

---
+++ Plugins/Accessibility/ScreenDivider/ScreenDivider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CK.Plugin;
+
+namespace ScreenDivider
+{
+    public class ScreenDivider : IPlugin
+    {
+        public bool Setup( IPluginSetupInfo info )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Teardown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

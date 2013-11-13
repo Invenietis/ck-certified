891fec56982b00d8860bae06258eb45b90276895
Idriss =) <ihippocrate@gmail.com>
2013-11-13 19:03:44 +0100

    plugin basics and registry explorer

---
+++ Plugins/Accessibility/FileLauncher/FileInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace FileLauncher
+{
+    public class FileInfo
+    {
+        public string FileName { get; private set; }
+        public string Path { get; private set; }
+        public string ProductName { get; private set; }
+        public Icon Icon { get; private set; }
+
+        public FileInfo(string path)
+        {
+            Path = path;
+            Console.WriteLine("File : " + path);
+            FileVersionInfo finfo = FileVersionInfo.GetVersionInfo(path);
+            FileName = finfo.FileName;
+            ProductName = finfo.ProductName;
+            Icon = Icon.ExtractAssociatedIcon(path);
+        }
+    }
+}
---
+++ Plugins/Accessibility/FileLauncher/FileLauncher.cs
@@ -0,0 +1,29 @@
+﻿using CK.Plugin;
+using CommonServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace FileLauncher
+{
+    [Plugin(FileLauncher.PluginIdString,
+           PublicName = PluginPublicName,
+           Version = FileLauncher.PluginIdVersion,
+           Categories = new string[] { "Visual", "Accessibility" })]
+    public class FileLauncher : BasicCommandHandler
+    {
+        const string PluginIdString = "{02D08D49-171F-454A-A84C-89DD7F959958}";
+        Guid PluginGuid = new Guid(PluginIdString);
+        const string PluginIdVersion = "1.0.0";
+        const string PluginPublicName = "File Launcher";
+
+        public override void Start()
+        {
+            base.Start();
+            var list = RegistryApplication.Crawl();
+            RegistryApplication.Launch(list.FirstOrDefault());
+        }
+    }
+}

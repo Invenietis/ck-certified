a759f73c11d2395ef99b0af02aba4ffc3a5211d2
Jean-Loup Kahloun <jean-loup.kahloun@invenietis.com>
2014-04-01 16:51:35 +0200

    Editor : bug fixing

---
+++ Plugins/Advanced/ContextEditor/KeyboardEdition/Components/Converters/EnsureIntegerConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace KeyboardEditor.ViewModels
+{
+    [ValueConversion( typeof( string ), typeof( int ) )]
+    public class EnsureIntegerConverter : IValueConverter
+    {
+        public object Convert( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
+        {
+            int finalVal = 0;
+            Int32.TryParse( value.ToString(), out finalVal );
+            return finalVal;
+        }
+
+        public object ConvertBack( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
+        {
+            int finalVal = 0;
+            Int32.TryParse( value.ToString(), out finalVal );
+            return finalVal;
+        }
+    }
+}

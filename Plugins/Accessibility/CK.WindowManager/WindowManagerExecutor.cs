6f72169e4bb44018da820253b96fc29822adaa54
Franck Bontemps <franck.bontemps@invenietis.com>
2014-06-05 15:24:38 +0200

    Adapted ActivityMonitor

--- Plugins/Accessibility/CK.WindowManager/WindowManagerExecutor.cs
+++ Plugins/Accessibility/CK.WindowManager/WindowManagerExecutor.cs
@@ -15,7 +15,7 @@ namespace CK.WindowManager
     public class WindowManagerExecutor : IPlugin
     {
         PreviewBindingInfo _placeholder;
-        DefaultActivityLogger _logger;
+        ActivityMonitor _logger;
 
         [DynamicService( Requires = RunningRequirement.MustExistAndRun )]
         public IWindowManager WindowManager { get; set; }
@@ -26,7 +26,7 @@ namespace CK.WindowManager
         public WindowManagerExecutor()
         {
             _placeholder = new PreviewBindingInfo();
-            _logger = new DefaultActivityLogger();
+            _logger = new ActivityMonitor();
             //_logger.Tap.Register( new ActivityLoggerConsoleSink() );
         }
 

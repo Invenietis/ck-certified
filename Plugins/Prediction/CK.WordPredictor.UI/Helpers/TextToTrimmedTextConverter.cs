61e371435f8af63aefc85b9542d84ae42a5a4143
Franck Bontemps <franck.bontemps@invenietis.com>
2014-09-18 14:07:45 +0200

    added trimming center for prediction textblock

--- Plugins/Prediction/CK.WordPredictor.UI/Helpers/TextToTrimmedTextConverter.cs
+++ Plugins/Prediction/CK.WordPredictor.UI/Helpers/TextToTrimmedTextConverter.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media;
+using CK.Windows;
 using CK.WPF.StandardViews;
 
 namespace CK.WordPredictor.UI.Helpers
@@ -22,9 +27,10 @@ namespace CK.WordPredictor.UI.Helpers
             FormattedText formatted;
 
             string text = (string)values[0];
-            StdKeyView view = (StdKeyView)values[1];
-
-            return text; // TODO
+            StdKeyView key = (StdKeyView)values[1];
+            TextBlock textblock = key.FindChildren<TextBlock>().FirstOrDefault( tb => tb.Name == "Letter" );
+            if( textblock == null ) return text; // TODO
+            return GetTrimmedPath( text, textblock, key );
         }
 
         public object[] ConvertBack( object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture )
@@ -33,5 +39,60 @@ namespace CK.WordPredictor.UI.Helpers
         }
 
         #endregion
+
+        string GetTrimmedPath( string text, TextBlock textblock, StdKeyView key )
+        {
+            FormattedText formatted = new FormattedText(
+                    text,
+                    CultureInfo.CurrentCulture,
+                    FlowDirection.LeftToRight,
+                    new Typeface( textblock.FontFamily,
+                                    textblock.FontStyle,
+                                    textblock.FontWeight,
+                                    textblock.FontStretch ),
+                    textblock.FontSize,
+                    textblock.Foreground
+                    textblock.ToolTip
+                );
+            bool widthOK = formatted.Width < key.ActualWidth;
+
+            if( !widthOK )
+            {
+                string part1;
+                string part2;
+                SplitStrings( text, out part1, out part2 );
+
+
+                while( !widthOK )
+                {
+                    part1 = part1.Substring( 0, part1.Length - 1 );
+                    part2 = part2.Substring( 1, part1.Length );
+
+                    formatted = new FormattedText(
+                        String.Format( "{0}…{1}", part1, part2 ),
+                        CultureInfo.CurrentCulture,
+                        FlowDirection.LeftToRight,
+                        new Typeface( textblock.FontFamily,
+                                        textblock.FontStyle,
+                                        textblock.FontWeight,
+                                        textblock.FontStretch ),
+                        textblock.FontSize,
+                        textblock.Foreground
+                    );
+
+                    widthOK = formatted.Width < key.ActualWidth && part1.Length != 0;
+                }
+            }
+
+            return formatted.Text;
+        }
+
+        private void SplitStrings( string text, out string part1, out string part2 )
+        {
+            int length = text.Length / 2;
+
+            part1 = text.Substring( 0, length );
+            part2 = text.Substring( text.Length - length, length );
+        }
     }
 }
